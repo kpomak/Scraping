737191f697563b590bb193ad43400756025d2b86
kpomak <romka_ne@mail.ru>
2023-01-13 01:30:33 +0300

    added superjob spider

--- vacancy_scrap/run.py
+++ vacancy_scrap/run.py
@@ -3,12 +3,14 @@ from scrapy.settings import Settings
 
 from vacancy_scrap import settings as vacancy_settings
 from vacancy_scrap.spiders.headhunter import HeadhunterSpider
+from vacancy_scrap.spiders.superjob import SuperjobSpider
 
 if __name__ == "__main__":
     settings = Settings()
     settings.setmodule(vacancy_settings)
 
     process = CrawlerProcess(settings)
-    process.crawl(HeadhunterSpider)
+    # process.crawl(HeadhunterSpider)
+    process.crawl(SuperjobSpider)
 
     process.start()
---
+++ vacancy_scrap/vacancy_scrap/spiders/superjob.py
@@ -0,0 +1,27 @@
+import scrapy
+from scrapy.http import HtmlResponse
+
+from vacancy_scrap.items import VacancyScrapItem
+
+
+class SuperjobSpider(scrapy.Spider):
+    name = 'superjob'
+    allowed_domains = ['superjob.ru']
+    start_urls = ['https://russia.superjob.ru/vacancy/search/?keywords=Python']
+
+    def parse(self, response: HtmlResponse):
+        vacancies = response.xpath("//div[contains(@class, 'f-test-vacancy-item')]/div/div[1]//a/@href").extract()
+        for vacancy in vacancies:
+            yield response.follow(vacancy, self.item_parse)
+
+        next_page = (
+            "https://russia.superjob.ru"
+            + response.xpath("//a[contains(@class, 'f-test-button-dalshe')]/@href").extract_first()
+        )
+
+        if next_page:
+            yield response.follow(next_page, self.parse)
+
+
+    def item_parse(self, response: HtmlResponse):
+        print(1)
