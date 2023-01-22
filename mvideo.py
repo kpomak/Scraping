63d09481a6c0c03af945b1dcb7275555b2a64b39
kpomak <romka_ne@mail.ru>
2023-01-22 16:26:47 +0300

    roll carusel

--- mvideo.py
+++ mvideo.py
@@ -1,9 +1,53 @@
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 
-service = Service("./geckodriver")
-browser = webdriver.Firefox(service=service)
-browser.get("https://mvideo.ru/")
+def initializer(url):
+    """
+    func launches firefox browser with geckodriver
+    """
+    (*RESOLUTION,) = 1920, 1080
 
+    service = Service("./geckodriver")
+    browser = webdriver.Firefox(service=service)
+    browser.set_window_size(*RESOLUTION)
+    browser.get(url)
+    return browser
+
+
+def target_handler(browser, target):
+    """
+    func tries to find span with target text
+    """
+    try:
+        link = browser.find_element(By.XPATH, f"//span[contains(text(),'{target}')]")
+    except NoSuchElementException:
+        link = None
+    return link
+
+
+def scroller(browser, target):
+    """
+    func scrolls web-page until finds span with target text
+    """
+    actions = ActionChains(browser)
+    link = target_handler(browser, target)
+    while not link:
+        actions.scroll_by_amount(0, 500).perform()
+        link = target_handler(browser, target)
+    return link
+
+
+url = "https://www.mvideo.ru/"
+browser = initializer(url)
+button = scroller(browser, target="В тренде")
+button.click()
+arrow_button = browser.find_elements(By.XPATH, '//mvid-shelf-group//button[contains(@class, "btn forward")]')[-1]
+arrow_button.click()
+print()
 browser.quit()
