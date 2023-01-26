d9718647df0d0243a0492fc6a74069d99b0ca183
kpomak <romka_ne@mail.ru>
2023-01-26 03:33:04 +0300

    logged in

--- insta/spiders/instagram.py
+++ insta/spiders/instagram.py
@@ -1,3 +1,5 @@
+import re
+
 import scrapy
 from scrapy.http import HtmlResponse
 
@@ -7,12 +9,37 @@ from insta import settings
 class InstagramSpider(scrapy.Spider):
     name = "instagram"
     allowed_domains = ["instagram.com"]
-    start_urls = ["http://instagram.com/"]
+    start_urls = ["https://www.instagram.com/"]
     login_url = "https://www.instagram.com/api/v1/web/accounts/login/ajax/"
-    ogin = settings.LOGIN
+    login = settings.LOGIN
     password = settings.PASSWORD
     accounts = settings.USERS_LIST
     api_url = "https://www.instagram.com/graphql/query/?"
 
     def parse(self, response: HtmlResponse):
-        pass
+        token = self.get_csrf(response.text)
+        yield scrapy.FormRequest(
+            self.login_url,
+            method="POST",
+            callback=self.parse_user,
+            headers={
+                "X-CSRFToken": token,
+            },
+            formdata={
+                "enc_password": self.password,
+                "username": self.login,
+                "queryParams": {},
+                "optIntoOneTap": "false",
+                "trustedDeviceRecords": {},
+            },
+        )
+
+    def parse_user(self, response: HtmlResponse):
+        json_data = response.json()
+        if json_data.get("authenticated"):
+            _id = json_data.get("userId")
+            print("well done")
+
+    def get_csrf(self, text):
+        token = re.search('csrf_token\\\\":\\\\"\w+', text).group()
+        return token.split('"').pop()
