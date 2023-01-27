6a154d6ea893e3894e2fd0ebc8ae038f4b93c7a5
kpomak <romka_ne@mail.ru>
2023-01-27 11:54:18 +0300

    save to db

--- insta/pipelines.py
+++ insta/pipelines.py
@@ -1,10 +1,29 @@
+import os
+
 import scrapy
+from pymongo import MongoClient
 from scrapy.pipelines.images import ImagesPipeline
 
 
 class InstaPipeline:
+    def __init__(self):
+        mongo_uri = os.getenv("MONGODB_URI")
+        client = MongoClient(mongo_uri)
+        db = client.social
+        self.collection = db.instagram
+
     def process_item(self, item, spider):
-        print(item)
+        record_user = self.collection.find_one({"_id": item["_id"]})
+        if not record_user:
+            self.collection.insert_one(item)
+        elif item.get("followers"):
+            self.collection.update_one(
+                {"_id": item["_id"]}, {"$push": {"followers": item["followers"].pop()}}
+            )
+        else:
+            self.collection.update_one(
+                {"_id": item["_id"]}, {"$push": {"following": item["following"].pop()}}
+            )
         return item
 
 
